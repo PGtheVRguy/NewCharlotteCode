2ed19dd6aa28a73eaa94dfeee2fcb3ffea1715e5
PGtheVRguy <sapelakn@gmail.com>
2023-05-17 18:13:20 -0400

    AA

---
+++ src/main/java/frc/robot/commands/DriveWithJoystickCommand.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveTrainSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class DriveWithJoystickCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final DriveTrainSubsystem driveTrainSubsystem;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public DriveWithJoystickCommand(DriveTrainSubsystem driveTrainSubsystem) {
+    this.driveTrainSubsystem = driveTrainSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveTrainSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    System.out.println("Starting this really cool joystick command");
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double forwardSpeed = RobotContainer.joystick.getX();
+    double turningSpeed = RobotContainer.joystick.getY();
+    driveTrainSubsystem.arcadeDrive(forwardSpeed, turningSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -0,0 +1,95 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+
+public class DriveTrainSubsystem extends SubsystemBase {
+
+  CANSparkMax leftMotor1 = new CANSparkMax(Constants.DriveTrainConstants.leftMotor1ID, MotorType.kBrushed);
+  CANSparkMax leftMotor2 = new CANSparkMax(Constants.DriveTrainConstants.leftMotor2ID, MotorType.kBrushed);
+  CANSparkMax leftMotor3 = new CANSparkMax(Constants.DriveTrainConstants.leftMotor3ID, MotorType.kBrushed);
+  CANSparkMax rightMotor1 = new CANSparkMax(Constants.DriveTrainConstants.rightMotor1ID, MotorType.kBrushed);
+  CANSparkMax rightMotor2 = new CANSparkMax(Constants.DriveTrainConstants.rightMotor2ID, MotorType.kBrushed);
+  CANSparkMax rightMotor3 = new CANSparkMax(Constants.DriveTrainConstants.rightMotor3ID, MotorType.kBrushed);
+
+  RelativeEncoder leftEncoder = leftMotor1.getEncoder();
+  RelativeEncoder rightEncoder = rightMotor1.getEncoder();
+
+
+  MotorControllerGroup leftControllerGroup = new MotorControllerGroup(leftMotor1, leftMotor2, leftMotor3);
+  MotorControllerGroup rightControllerGroup = new MotorControllerGroup(rightMotor1, rightMotor2, rightMotor3);
+
+  DifferentialDrive differentialDrive = new DifferentialDrive(leftControllerGroup, rightControllerGroup);
+
+  public DriveTrainSubsystem(){ 
+
+    leftMotor1.restoreFactoryDefaults();
+    leftMotor2.restoreFactoryDefaults();
+    leftMotor3.restoreFactoryDefaults();
+    rightMotor1.restoreFactoryDefaults();
+    rightMotor2.restoreFactoryDefaults();
+    rightMotor3.restoreFactoryDefaults();
+
+
+
+    leftEncoder.setPosition(0);
+    rightEncoder.setPosition(0);
+
+    leftMotor2.follow(leftMotor1);
+    leftMotor3.follow(leftMotor1);
+  
+    rightMotor2.follow(rightMotor1);
+    rightMotor3.follow(rightMotor1);
+
+
+    rightControllerGroup.setInverted(true);
+    leftControllerGroup.setInverted(false);
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public CommandBase exampleMethodCommand() {
+
+    return runOnce(
+        () -> {
+
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+  public void arcadeDrive(double fwd, double rot) {
+    differentialDrive.arcadeDrive(fwd, rot);
+  }
+}
